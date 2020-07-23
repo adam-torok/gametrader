5eccab59437b05032a232f1034fd461d1434b65b
adam-torok <adam-torok@outlook.hu>
2020-07-23 22:24:38 +0200

    'pushnoti'

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,29 @@
 <template>
   <div id="app">
-    <Navbar></Navbar>
-    <transition name="router-anim" enter-active-class="animated fadeIn" leave-active-class="animated fadeOut">
-      <router-view/>
-    </transition>
-    <Footer></Footer>
+  <Navbar></Navbar>
+  <transition name="router-anim" enter-active-class="animated fadeIn" leave-active-class="animated fadeOut">
+    <router-view/>
+  </transition>
+  <Footer></Footer>
   </div>
 </template>
-
 <script>
 import Navbar from '@/components/Navbar'
+import API from '@/api/api'
 import Footer from '@/components/Footer'
 export default {
   name: 'App',
   components:{
-    Navbar,
-    Footer
+  Navbar,
+  Footer
+  },
+  async mounted(){
+    localStorage.setItem('isNew','0');
+    if (Notification.permission !== 'granted')
+    Notification.requestPermission();
+    await setInterval(() => {
+      API.doPoll();
+    }, 5000);
   }
 }
 </script>
--- src/api/api.js
+++ src/api/api.js
@@ -4,62 +4,88 @@ const API_URL_UPLOAD = 'http://192.168.1.8:80/restapi/api/advs/upload_image.php'
 const API_URL_GETSINGLE = 'http://192.168.1.8:80/restapi/api/advs/read_single.php?id=';
 const API_URL_LOGINUSER = 'http://192.168.1.8:80/restapi/api/user/login.php';
 const API_URL_REGISTRATEUSER = 'http://192.168.1.8:80/restapi/api/user/registrate.php';
+const API_NOTIFY = 'http://192.168.1.8:80/restapi/api/notifications/noti.php';
+const API_STORAGE = 'http://192.168.1.8:80/restapi/storage/';
+const APP_SINGLEADVERT = 'http://localhost:8080/singleadvert/';
 
 
 export default{
-    async fetchAdverts() {
-    console.log('fetching...');
-    const res = await fetch(API_URL);    
-    return res.json();
-    },
-    async insertAdvert(advert) {
-        await fetch(
-        API_URL_INSERT,
-        {
-            method: 'POST',
-            "mimeType": "multipart/form-data",
-            body: JSON.stringify(advert)
-        }
-    );
-    },
-    async uploadAdvertImage(image){
-        var form_data = new FormData();                  
-        form_data.append('advert_picture', image);
-        
-        const response = await fetch(
-            API_URL_UPLOAD,
-            {
-                method: 'POST',
-                "mimeType": "multipart/form-data",
-                body: form_data             
-            }
-        );
-        const data = await response.text();
-        return data;
-    },
-    async getAdvert(id) {
-        const res = await fetch(API_URL_GETSINGLE+id.id);   
-        return res.json();
-    },
-    loginUser(user){
-        // TODO make login authentication work, If authentication is correct, send back cookies 
-        console.log(user);
-        const res = fetch(API_URL_LOGINUSER,{
-            method: 'POST',
-            body: user  
-            }
-        );
-        return res;
-    },
-    async registrateUser(user){
-        console.log(user);
-        await fetch(
-            API_URL_REGISTRATEUSER,
-            {
-                method: 'POST',
-                "mimeType": "multipart/form-data",
-                body: JSON.stringify(user)
-            }
-        );
+  async fetchAdverts() {
+  const res = await fetch(API_URL);    
+  return res.json();
+  },
+  async insertAdvert(advert) {
+    await fetch(
+    API_URL_INSERT,
+    {
+      method: 'POST',
+      "mimeType": "multipart/form-data",
+      body: JSON.stringify(advert)
     }
+  );
+  },
+  async uploadAdvertImage(image){
+    var form_data = new FormData();                  
+    form_data.append('advert_picture', image);
+    
+    const response = await fetch(
+      API_URL_UPLOAD,
+      {
+        method: 'POST',
+        "mimeType": "multipart/form-data",
+        body: form_data             
+      }
+    );
+    const data = await response.text();
+    return data;
+  },
+  async getAdvert(id) {
+    const res = await fetch(API_URL_GETSINGLE+id.id);   
+    return res.json();
+  },
+  loginUser(user){
+    const res = fetch(API_URL_LOGINUSER,{
+      method: 'POST',
+      body: user  
+      }
+    );
+    return res;
+  },
+  async registrateUser(user){
+    console.log(user);
+    await fetch(
+      API_URL_REGISTRATEUSER,
+      {
+        method: 'POST',
+        "mimeType": "multipart/form-data",
+        body: JSON.stringify(user)
+      }
+    );
+  },
+  async doPoll(){
+    fetch(API_NOTIFY)
+    .then(response => response.json())
+    .then(data => {
+      if(data.notify){
+        let current_id = data.id;
+        if(current_id != localStorage.getItem('isNew')){ 
+          localStorage.setItem('isNew',data.id);
+          var notification = new Notification(data.title, {
+            icon: 'https://boldicenter.com/img/ikon_newest.png',
+            body: data.desc,
+            tag : 'Új hirdetés',
+            image : API_STORAGE+data.image
+          });    
+          notification.onclick = function () {
+            event.preventDefault();
+            window.open(APP_SINGLEADVERT+data.id, '_blank');
+          };
+          notification.onshow = function(){
+            console.log("Valami");
+          }
+          }
+        }
+      }
+    );
+  },
 }
